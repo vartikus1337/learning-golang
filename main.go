1d8892b72877a67032f59799915d5cbb355e92ba
vartikus1337 <pasha_klevtsov@vk.com>
2023-12-05 21:33:20 +0300

    typical variables

--- main.go
+++ main.go
@@ -3,5 +3,31 @@ package main
 import "fmt"
 
 func main() {
-    fmt.Println("Hello World!")
+
+	var name string = "any name"
+	var age = 18 // or age := 18
+
+	//  or
+
+	// var (
+	// 	name string = "any name"
+	// 	age  int    = 18
+	// )
+
+	var symbol int32 = 'g'
+
+	fmt.Println("Hello " + name)
+	fmt.Println("Your age:", age, "\nsymbol (char)-<int>:", symbol, "\nsymbol (char)-<string>:", string(symbol))
+
+	a := 10
+
+	f := 2.0
+
+	fmt.Println("rounded division:", a/3)
+	fmt.Println("surplus division:", a%3)
+	a++
+	fmt.Println("++", a)
+	a--
+	fmt.Println("--", a)
+	fmt.Println("float division", f/3)
 }
