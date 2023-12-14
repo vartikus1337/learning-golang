8ca5112925ae416b67f723517e1e30179b549636
vartikus1337 <pasha_klevtsov@vk.com>
2023-12-14 18:07:09 +0300

    Data type conversion

--- main.go
+++ main.go
@@ -1,19 +1,60 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
-func main() {
-	m := map[int]string{
-		1: "one",
-		2: "two",
-		3: "three",
+func ok() {
+	age := 1337
+	fmt.Println("Привет мне " + strconv.Itoa(age) + " лет")
+	// или res, err := strconv.ParseInt(age, 10, 0)
+
+	var f float64 = 1.3223
+	// 1 параметр - число для конвертации
+	// fmt - форматирование
+	// prec - точность (кол-во знаков после запятой)
+	// bitSize - 32 или 64 (32 для float32, 64 для float64)
+	fmt.Println(strconv.FormatFloat(f, 'f', 2, 64)) // 1.01
+
+	// Возможные форматы fmt:
+	// 'f' (-ddd.dddd, no exponent),
+	// 'b' (-ddddp±ddd, a binary exponent),
+	// 'e' (-d.dddde±dd, a decimal exponent),
+	// 'E' (-d.ddddE±dd, a decimal exponent),
+	// 'g' ('e' for large exponents, 'f' otherwise),
+	// 'G' ('E' for large exponents, 'f' otherwise),
+	// 'x' (-0xd.ddddp±ddd, a hexadecimal fraction and binary exponent), or
+	// 'X' (-0Xd.ddddP±ddd, a hexadecimal fraction and binary exponent).
+	var fb float64 = 2222 * 1023 * 245 * 2 * 52
+	fmt.Println(strconv.FormatFloat(fb, 'e', -1, 64)) // 5.791874088e+10
+
+	//  https://yourbasic.org/golang/fmt-printf-reference-cheat-sheet/
+
+	var b bool = true
+	fmt.Println(strconv.FormatBool(b))
+
+	str1 := "10"
+	str2 := "20"
+
+	str1Int, err := strconv.Atoi(str1)
+	if err != nil {
+		fmt.Println(err)
 	}
-	delete(m, 1)
-	fmt.Println(m)
-	if value, inMap := m[1]; inMap {
-		fmt.Println(value)
+
+	str2Int, err := strconv.Atoi(str2)
+	if err != nil {
+		fmt.Println(err)
 	}
-	for key, value := range m {
-		fmt.Println(key, value)
-	}
+	fmt.Println(str1Int - str2Int)
+
+	float := "1.0000000012345678"
+	result32, _ := strconv.ParseFloat(float, 32)
+	result64, _ := strconv.ParseFloat(float, 64)
+	fmt.Println("bitSize32:", result32)  // вывод 1 (не уместились)
+	fmt.Println("bitSize64:", result64)  // вывод  1.0000000012345678
+
+	str3 := "true"
+	res, _ := strconv.ParseBool(str3)
+	fmt.Printf("%T \n", res)
 }
