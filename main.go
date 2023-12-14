3e60e221016922c6dd75002931658600d4e18ada
vartikus1337 <pasha_klevtsov@vk.com>
2023-12-14 17:08:51 +0300

    map

--- main.go
+++ main.go
@@ -1,42 +1,19 @@
 package main
 
-import (
-	"errors"
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	var input int
-	_, err := fmt.Scan(&input)
-	if err!= nil {
-        fmt.Println("Проверьте типы входных параметров")
-    } else {
-		fmt.Println(input * 3)
+	m := map[int]string{
+		1: "one",
+		2: "two",
+		3: "three",
 	}
-	// test_error()
-
-	err_test := error_test()
-	if err_test != nil {
-		fmt.Println(err_test.Error())
+	delete(m, 1)
+	fmt.Println(m)
+	if value, inMap := m[1]; inMap {
+		fmt.Println(value)
 	}
-}
-
-func test_error() {
-	err := errors.New("my error")
-    fmt.Println("", err)
-}
-
-func error_test() error {
-	i := 0
-	fmt.Println("Выполнение функции error_test() 1")
-	i++ // 1
-	defer deferTest(i) // Попало 1
-	i++ // 2
-	fmt.Println("Выполнение функции error_test() 2")
-	return errors.New("ошибка в error_test()")
-}
-
-func deferTest(i int) {
-	fmt.Println("Финал error_test()")
-	fmt.Println("i:", i)
+	for key, value := range m {
+		fmt.Println(key, value)
+	}
 }
