2ee8c9a85cf3cc656977c56c6565df5b17add334
vartikus1337 <pasha_klevtsov@vk.com>
2023-12-15 19:27:17 +0300

    Working with files - Part 1

--- main.go
+++ main.go
@@ -1,53 +1,49 @@
-// Давайте используем ваши знания структур, методов и интерфейсов на практике и реализуем объект,
-// удовлетворяющий интерфейсу fmt.Stringer. Назовем его "Батарейка".
-
-// Во-первых, вы должны объявить новый тип, удовлетворяющий интерфейсу fmt.Stringer.
-
-// Ваш тип должен предусматривать, что на печати он будет выглядеть так: [      XXXX]:
-// где пробелы - "опустошенная" емкость батареи, а X - "заряженная".
-
-// Во-вторых, на стандартный ввод вы получаете строку, состоящую ровно из 10 цифр:
-// 0 или 1 (порядок 0/1 случайный). Ваша задача считать эту строку любым возможным способом и создать на основе этой строки объект объявленного вами на первом этапе типа:
-// надеюсь, вы понимаете, что строка символизирует емкость батарейки: 0 - это "опустошенная" часть, а 1 - "заряженная".
-
-// В-третьих, созданный вами объект должен называться batteryForTest (использование этого имени обязательно).
-
-//  1000010011 -> [      XXXX]
-
 package main
 
 import (
+	"bufio"
 	"fmt"
-    "strings"
+	"io"
+	"os"
+	"strconv"
 )
 
-type Buttery struct {
-    charge string        
-}
-
-func (b Buttery) String() string {
-	var chargeFormatted string
-	chargeFormatted += strings.Repeat(" ", strings.Count(b.charge, "0"))
-	chargeFormatted += strings.Repeat("X", strings.Count(b.charge, "1"))
-    return fmt.Sprintf("[%v]", chargeFormatted)
-}
-
 func main() {
-	var charge string
-	fmt.Scan(&charge)
-	batteryForTest := Buttery{charge}
-	fmt.Println(batteryForTest)
+	file1, _ := os.Create("text.txt")
+	file1.WriteString("1 строка \n")
+	info1, _ := file1.Stat()
+	fmt.Printf("Size: %d\n", info1.Size())
+	os.Rename("text.txt", "new_text.txt")
+	os.Remove("new_text.txt")
+
+	var sum int
+    s := bufio.NewScanner(os.Stdin)
+    for s.Scan() {
+        num, _  := strconv.Atoi(s.Text())
+        sum += num 
+    }
+    io.WriteString(os.Stdout, strconv.Itoa(sum))
+
 }
 
-//  Another solution:
+func anotherSolution() {
+	reader := bufio.NewReader(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
 
-type battery string
+	var sum int
+	for {
+		line, _, err := reader.ReadLine()
 
-func (b battery) String() string {
-    return fmt.Sprintf("[%10s]", strings.Repeat("X", strings.Count(string(b), "1")))
+		if err == io.EOF {
+			break
+		}
+
+		num, _ := strconv.Atoi(string(line))
+		sum += num
+	}
+
+	writer.WriteString(strconv.Itoa(sum))
+	writer.Flush()
 }
 
-func main1() {
-    var batteryForTest battery
-    fmt.Scan(&batteryForTest)
-}
+
