a72f224eeff72a241b3f756dcf3555cd572f266b
vartikus1337 <pasha_klevtsov@vk.com>
2023-12-15 17:14:54 +0300

    Interfaces - String() Error()

--- main.go
+++ main.go
@@ -1,97 +1,41 @@
-// Задание https://stepik.org/lesson/350788/step/10?unit=334666
-
 package main
 
 import "fmt"
 
 
-func readTask() (interface{}, interface{}, interface{}) {
-	return interface{}(1.123123), interface{}(2.7343), interface{}("*")
+type Person struct {
+	name string
+	age uint
+}
+
+func (p *Person) String() string {
+	return fmt.Sprintf("%v (%d)", p.name, p.age)
+}
+
+type PersonErrorAge int
+
+func (e PersonErrorAge) Error() string {
+    return fmt.Sprintf("Ошибка в указании возраста: %d", e)
+}
+
+func SetAge(age int, p *Person) error {
+	if age < 0 {
+		return PersonErrorAge(age)
+	}
+	return nil
 }
 
 func main() {
-	value1, value2, operation := readTask() // исходные данные получаются с помощью этой функции
-                                            // все полученные значения имеют тип пустого интерфейса
-    if _, ok := value1.(float64); !ok {
-        fmt.Printf("value=%v: %T", value1, value1)
-        return 
-    }
-    if _, ok := value2.(float64); !ok {
-        fmt.Printf("value=%v: %T", value2, value2)
-        return 
-    }
-    if _, ok := operation.(string); !ok {
-        fmt.Println("неизвестная операция")
-        return
-    }
-    switch o := operation.(string); o {
-        case "+":
-            fmt.Printf("%.4f", value1.(float64) + value2.(float64))
-        case "-":
-            fmt.Printf("%.4f", value1.(float64) - value2.(float64))
-        case "*":
-            fmt.Printf("%.4f", value1.(float64) * value2.(float64))
-        case "/":
-            fmt.Printf("%.4f", value1.(float64) / value2.(float64))
-        default:
-            fmt.Println("неизвестная операция")
-    }
-}
-
-func anotherSolution() {
-	value1, value2, operation := readTask()
-
-	v1, ok := value1.(float64)
-	if !ok {
-		fmt.Printf("value=%v: %T", value1, value1)
-		return
+	p := &Person{
+		name: "John",
+		age: 30,
 	}
-
-	v2, ok := value2.(float64)
-	if !ok {
-		fmt.Printf("value=%v: %T", value2, value2)
-		return
+	fmt.Println(p)
+    err := SetAge(-10, p)
+	if err != nil {
+		fmt.Printf("Ошибка обработана: %v\n", err)
 	}
-
-	switch operation.(string) {
-	case "+":
-		fmt.Printf("%.4f", v1+v2)
-	case "-":
-		fmt.Printf("%.4f", v1-v2)
-	case "*":
-		fmt.Printf("%.4f", v1*v2)
-	case "/":
-		fmt.Printf("%.4f", v1/v2)
-	default:
-		fmt.Println("неизвестная операция")
+	if cError, ok := err.(PersonErrorAge); ok {
+		fmt.Printf("Контекст: %d\n", cError)
 	}
 }
-func test() {
-	var i1 interface{} = true
-	var i2 interface{} = 2.0120012010210
-	var operation interface{} = "/"
-
-	
-
-	if _, ok := i1.(float64); !ok {
-        fmt.Printf("value=%v: %T", i1, i1)
-        return 
-    }
-    if f2, ok := i2.(float64); !ok {
-        fmt.Printf("value=%v;%T", f2, f2)
-        return 
-    }
-    if _, ok := operation.(string); !ok {
-        fmt.Println("неизвестная операция")
-        return
-    }
-
-	switch o := operation.(string); o {
-		case "/":
-			fmt.Printf("%.4f", i1.(float64) / i2.(float64))
-		default:
-			fmt.Println("неизвестная операция")
-	}
-
-}
-
