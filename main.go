36ba0a5da17a760eaf98372690db2ad67ba2dc86
vartikus1337 <pasha_klevtsov@vk.com>
2023-12-16 23:26:11 +0300

    json

--- main.go
+++ main.go
@@ -1,49 +1,82 @@
 package main
 
 import (
-	"bufio"
+	"bytes"
+	"encoding/json"
 	"fmt"
-	"io"
-	"os"
-	"strconv"
 )
 
-func main() {
-	file1, _ := os.Create("text.txt")
-	file1.WriteString("1 строка \n")
-	info1, _ := file1.Stat()
-	fmt.Printf("Size: %d\n", info1.Size())
-	os.Rename("text.txt", "new_text.txt")
-	os.Remove("new_text.txt")
-
-	var sum int
-    s := bufio.NewScanner(os.Stdin)
-    for s.Scan() {
-        num, _  := strconv.Atoi(s.Text())
-        sum += num 
-    }
-    io.WriteString(os.Stdout, strconv.Itoa(sum))
-
-}
-
-func anotherSolution() {
-	reader := bufio.NewReader(os.Stdin)
-	writer := bufio.NewWriter(os.Stdout)
-
-	var sum int
-	for {
-		line, _, err := reader.ReadLine()
-
-		if err == io.EOF {
-			break
-		}
-
-		num, _ := strconv.Atoi(string(line))
-		sum += num
+func main1() {
+	// JSON encoding работает с только заглавной буквой
+	type myStruct struct {
+		Name   string
+		Age    int
+		Status bool
+		Values []int
 	}
 
-	writer.WriteString(strconv.Itoa(sum))
-	writer.Flush()
+	s := myStruct{
+		Name:   "John Connor",
+		Age:    35,
+		Status: true,
+		Values: []int{15, 11, 37},
+	}
+
+	// Функция Marshal принимает аргумент типа interface{} (в нашем случае это структура)
+	// и возвращает байтовый срез с данными, кодированными в формат JSON.
+
+	data, err := json.Marshal(s) // {"Name":"John Connor","Age":35,"Status":true,"Values":[15,11,37]}
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Printf("%s", data)
+
+	data, err = json.MarshalIndent(s, "", "\t")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("%s \n", data)
+
+	data = []byte(`{"Name":"John 123","Age":123,"Status":false,"Values":[1,2,3]}`)
+	var sNew myStruct
+	if err := json.Unmarshal(data, &sNew); err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("%v", sNew) // {John 123 123 false [1 2 3]}
+
+	data = bytes.Trim(data, "{")
+
+	if !json.Valid(data) {
+		fmt.Println("invalid json!") // вывод: invalid json!
+	}
+	fmt.Printf("%s \n", data)
+
+	// Annotating Structures
+
+	type myStructAnn struct {
+		// при кодировании / декодировании будет использовано имя name, а не Name
+		Name string `json:"name"`
+
+		// при кодировании / декодировании будет использовано то же имя (Age),
+		// но если значение поля равно 0 (пустое значение: false, nil, пустой слайс и пр.),
+		// то при кодировании оно будет опущено
+		Age int `json:",omitempty"`
+
+		// при кодировании / декодировании поле всегда игнорируется
+		Status bool `json:"-"`
+	}
+
+	m := myStructAnn{Name: "John Connor", Age: 0, Status: true}
+
+	data, err = json.Marshal(m)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("%s \n", data) // {"name":"John Connor"}
+
 }
-
-
