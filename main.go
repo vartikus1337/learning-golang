2175cf2e8e881aa46515ad623de83ce0f4878b88
vartikus1337 <pasha_klevtsov@vk.com>
2023-12-18 20:48:29 +0300

    Parallelism - sync.WaitGroup

--- main.go
+++ main.go
@@ -1,64 +1,53 @@
 package main
 
-import "fmt"
-
-func main1() {
-	done := make(chan struct{}) // Канал используется для синхронизации
-	go myFunc1(done)
-	<-done
-}
-
-func myFunc1(done chan struct{}) {
-	fmt.Println("hello")
-	close(done)
-}
-
-// Для синхронизации выведен шаблон:
+import (
+	"fmt"
+	"sync"
+	"time"
+)
 
 func main() {
-	<-myFunc()
+	wg := new(sync.WaitGroup)
+
+	for i := 0; i < 5; i++ {
+		wg.Add(1)      // Увеличиваем счетчик горутин в группе
+		go work(i, wg) // Вызываем функцию work в отдельной горутине
+	}
+
+	wg.Wait() // ожидаем завершения всех горутин в группе
+	fmt.Println("Горутины завершили выполнение")
 }
 
-func myFunc() <-chan struct{} {
-	done := make(chan struct{})
-	go func() {
-		fmt.Println("hello")
-		close(done)
-	}()
-	return done
+func work(id int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	fmt.Printf("Горутина %d начала выполнение \n", id)
+	time.Sleep(2 * time.Second)
+	fmt.Printf("Горутина %d завершила выполнение \n", id)
 }
 
-//  канал для синхронизации создается самой функцией myFunc(), полезная работа выполняется в отдельной горутине
-// <-chan struct{}. Стрелка слева от ключевого слова chan означает, что возвращаемый канал предназначен только для чтения из него
-//  вернуть из функции или передать в нее в качестве аргумента канал, предназначенный только для записи - chan<- struct{}
-
-// Внутри функции main (функцию объявлять не нужно), вам необходимо в отдельной горутине вызвать функцию work() и дождаться результатов ее выполнения.
-// Функция work() ничего не принимает и не возвращает. https://stepik.org/lesson/345547/step/3?thread=solutions&unit=329291
+// Внутри функции main (функцию объявлять не нужно), вам необходимо в отдельных горутинах вызвать функцию work() 10 раз и дождаться результатов выполнения вызванных функций.
+// https://stepik.org/lesson/345547/step/5?thread=solutions&unit=329291
 
 func answer() {
-	done := make(chan struct{})
-	go func() {
-		// work()
-		close(done) // Лучше делать defer close(done) (на будущее)
-	}()
-	<-done
+	wg := new(sync.WaitGroup)
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func(wg *sync.WaitGroup) {
+			defer wg.Done()
+			// work()
+		}(wg)
+	}
+	wg.Wait()
 }
 
-//  как синхронизировать не синхронизированное)
-func explanation() {
-	done := make(chan struct{}) // создаем канал
-	go func(ch chan struct{}) { // создаем анонимную горутину
-		// work()
-		close(ch)               // канал закроется после выполнения work()
-	}(done)                     // передаем канал в анонимную горутину и запускаем ее
-	<-done
-}
-
-func explanationReboot() {
-	done := make(chan struct{})
-	go func() {
-		// work()
-        close(done)
-    }()
-	<-done
+func correctAnswer() {
+	wg := &sync.WaitGroup{}
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func(*sync.WaitGroup) {
+			defer wg.Done()
+			// work()
+		}(wg)
+	}
+	wg.Wait()
 }
