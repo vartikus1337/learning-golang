78e53976ec4c8164d174971fdf70fb6179aee4d3
vartikus1337 <pasha_klevtsov@vk.com>
2023-12-18 19:36:03 +0300

    Parallelism - task 3 inputStream outputStream

--- main.go
+++ main.go
@@ -1,4 +1,10 @@
-//  Напишите функцию которая принимает канал и строку. Необходимо отправить эту же строку в заданный канал 5 раз, добавив к ней пробел.
+// Напишите элемент конвейера (функцию), что запоминает предыдущее значение и отправляет значения на следующий этап конвейера
+//  только если оно отличается от того, что пришло ранее.
+
+// Ваша функция должна принимать два канала - inputStream и outputStream, в первый вы будете получать строки, во второй вы должны отправлять
+//  значения без повторов. В итоге в outputStream должны остаться значения, которые не повторяются подряд. Не забудьте закрыть канал ;)
+
+// Функция должна называться removeDuplicates()
 
 package main
 
@@ -6,25 +12,91 @@ import (
 	"fmt"
 )
 
-func task(c chan int, N int) {
-	c <- N + 1
-}
-
-func task2(c chan string, s string) {
-	for i := 0; i < 5; i++ {
-		c <- s + " "
+func fillStream(c chan string, s ...string) {
+	for _, s := range s {
+		c <- s
 	}
-
+	close(c)
 }
 
-func task2AnotherWay(c chan string, s string) {
-	for _, res := range "     " {
-		c <- s + string(res)
+func anotherFillInMainFunc() {
+	inputStream := make(chan string)
+	outputStream := make(chan string)
+	go removeDuplicates(inputStream, outputStream)
+
+	go func() {
+		defer close(inputStream)
+
+		for _, r := range "112334456" {
+			inputStream <- string(r)
+		}
+	}()
+
+	for x := range outputStream {
+		fmt.Print(x)
+	}
+}
+
+func seeInChan(c chan string) {
+	for value := range c {
+		fmt.Println(value)
 	}
 }
 
 func main() {
-	channel := make(chan string, 5)
-	task2(channel, "hi")
-	fmt.Println(<-channel)
+	inputStream := make(chan string)
+	outputStream := make(chan string)
+	go fillStream(inputStream, "a", "b", "d", "b", "b", "d")
+	go removeDuplicates(inputStream, outputStream)
+	seeInChan(outputStream)
+}
+
+// Не увидел что при вводе они идут отсортированными (у других под это решение)
+// Поэтому проверял на дупликацию элементов которые уже прошли
+// Получается перевыполнил)
+
+func removeDuplicates(inputStream chan string, outputStream chan string) {
+	var wall []string
+	for value := range inputStream {
+		if contains(wall, value) == false {
+			wall = append(wall, value)
+		}
+	}
+	for _, value := range wall {
+		outputStream <- value
+	}
+	close(outputStream)
+}
+
+func contains(wall []string, value string) bool {
+	for _, v := range wall {
+		if v == value {
+			return true
+		}
+	}
+	return false
+}
+
+// Как другие работают с потоками:
+
+func removeDuplicates2(in chan string, out chan string) {
+	var buf string
+	for v := range in {
+		if buf != v {
+			out <- v
+		}
+		buf = v
+	}
+	close(out)
+}
+
+func removeDuplicates3(inputStream, outputStream chan string) {
+	var prev string
+	for v := range inputStream {
+		if v != prev {
+			outputStream <- v
+			prev = v
+		}
+	}
+	close(outputStream)
 }
